7842053a6a0791ff5fd4dd2b1be946472430fff5
AbuTurab <68259771+abuturabofficial@users.noreply.github.com>
2024-12-12 11:01:36 +0500

    Initial hugo site with relearn theme

---
+++ content/_index.md
@@ -0,0 +1,5 @@
++++
+date = '2024-12-12T10:58:16+05:00'
+draft = false
+title = 'Test'
++++
---
+++ public/searchindex.en.js
@@ -0,0 +1,26 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Test",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "Test",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "Test",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
